c7157ba280dd8f158735892e1ab244bb20af6e53
Fahim Ali Zain TP <faztp12@gmail.com>
2017-02-02 17:34:31 +0530

    Update api.py

--- siyar_erpnext/api.py
+++ siyar_erpnext/api.py
@@ -4,4 +4,4 @@ import frappe
 def get_customer_item_discPercent(customer, item):
 	item_doc = frappe.get_doc("Item", item)
 	item_customer_detail = item_doc.get("customer_items", {"customer_name" : customer})	
-	return item_customer_detail[0].consoleerp_discpercent if item_customer_detail else 0
+	return item_customer_detail[0].consoleerp_discpercent if item_customer_detail[0] else 0
