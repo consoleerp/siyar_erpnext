1453d05e974b8d2478b757e5124c2fde5196b79c
Console Admin <faztp12@gmail.com>
2017-02-26 09:13:11 +0300

    Sales Invoice Bug

--- siyar_erpnext/fixtures/custom_scripts/Sales Invoice.js
+++ siyar_erpnext/fixtures/custom_scripts/Sales Invoice.js
@@ -1,39 +1,42 @@
 frappe.ui.form.on('Sales Invoice', {
 	onload : function(frm) {		
+	
+		frappe.after_ajax(function() {
 
-		// following conditions assures its a follow up document
-		if (frm.doc && frm.doc.customer && frm.doc.items && frm.doc.items.length > 0 &&frm.doc.items[0].item_code)
-		{
-			
-			// onload
-			// foreach items, copy current rate to customer rate and apply discount on actual rate
-			$.each(frm.doc.items, function(i, item_doc){
-
-				if (!item_doc.rate || item_doc.consoleerp_customer_rate)
-					return;
-								
-				frappe.model.set_value("Sales Invoice Item", item_doc.name, "consoleerp_customer_rate", item_doc.rate);	
+			// following conditions assures its a follow up document
+			if (frm.doc && frm.doc.customer && frm.doc.items && frm.doc.items.length > 0 &&frm.doc.items[0].item_code)
+			{
 				
-				frappe.after_ajax(function(){
+				// onload
+				// foreach items, copy current rate to customer rate and apply discount on actual rate
+				$.each(frm.doc.items, function(i, item_doc){
+
+					if (!item_doc.rate || item_doc.consoleerp_customer_rate)
+						return;
+									
+					frappe.model.set_value("Sales Invoice Item", item_doc.name, "consoleerp_customer_rate", item_doc.rate);	
 					
-					frappe.call({
-						"method" : "siyar_erpnext.api.get_customer_item_disc_percent",
-						args : {
-							customer : frm.doc.customer,
-							item : item_doc.item_code
-						},
-						callback : function(r) {
-							if (!r.message)
-								r.message = 0;
-							
-							frappe.model.set_value("Sales Invoice Item", item_doc.name, "consoleerp_customer_disc_percent", r.message);		
-							frappe.model.set_value("Sales Invoice Item", item_doc.name, "rate", item_doc.consoleerp_customer_rate * (100 - item_doc.consoleerp_customer_disc_percent) / 100);							
-						}
+					frappe.after_ajax(function(){
+						
+						frappe.call({
+							"method" : "siyar_erpnext.api.get_customer_item_disc_percent",
+							args : {
+								customer : frm.doc.customer,
+								item : item_doc.item_code
+							},
+							callback : function(r) {
+								if (!r.message)
+									r.message = 0;
+								
+								frappe.model.set_value("Sales Invoice Item", item_doc.name, "consoleerp_customer_disc_percent", r.message);		
+								frappe.model.set_value("Sales Invoice Item", item_doc.name, "rate", item_doc.consoleerp_customer_rate * (100 - item_doc.consoleerp_customer_disc_percent) / 100);							
+							}
+						});
+						
 					});
-					
 				});
-			});
-		}
+			}
+		});
 	},
 	
 	validate: function(frm) {
