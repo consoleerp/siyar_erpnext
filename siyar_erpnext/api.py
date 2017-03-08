b965fef9ca32f277b92030cd4795a344369f0dc1
Console Admin <faztp12@gmail.com>
2017-03-08 05:54:46 +0300

    bug with multiple frappe calls

--- siyar_erpnext/api.py
+++ siyar_erpnext/api.py
@@ -1,19 +1,24 @@
-import frappe
-
-@frappe.whitelist()
-def get_customer_items_discPercent(customer, item):
-	item_doc = frappe.get_doc("Item", item)
-	item_customer_detail = item_doc.get("customer_items", {"customer_name" : customer})	
-	return item_customer_detail[0].consoleerp_discpercent if item_customer_detail else None
-	
+import frappe, ast
 	
 @frappe.whitelist()
-def get_customer_item_disc_percent(customer, item):	
-	item_group = frappe.get_value(doctype="Item", filters={"name" : item}, fieldname="item_group")
-	if not item_group:
-		return 0	
+def get_customer_item_disc_percent(customer, items):	
 	
-	item_group_doc = frappe.get_doc("Item Group", item_group);
 	customer_group = frappe.get_value(doctype="Customer", filters={"name" : customer}, fieldname="customer_group")
-	discount_row = item_group_doc.get("consoleerp_custgroup_discount", {"customer_group" : customer_group})	
-	return discount_row[0].disc_percent if discount_row else None
+	
+	print customer
+	print items
+	items = ast.literal_eval(items)
+	print items
+	
+	values = []
+	for item in items:
+		print item
+		item_group = frappe.get_value(doctype="Item", filters={"name" : item}, fieldname="item_group")
+		if not item_group:
+			continue	
+	
+		item_group_doc = frappe.get_doc("Item Group", item_group)
+		discount_row = item_group_doc.get("consoleerp_custgroup_discount", {"customer_group" : customer_group})	
+		values.append(discount_row[0].disc_percent if discount_row else 0)
+	
+	return values
--- siyar_erpnext/fixtures/custom_scripts/Sales Invoice.js
+++ siyar_erpnext/fixtures/custom_scripts/Sales Invoice.js
@@ -3,38 +3,42 @@ frappe.ui.form.on('Sales Invoice', {
 	
 		frappe.after_ajax(function() {
 
-			// following conditions assures its a follow up document
-			if (frm.doc && frm.doc.customer && frm.doc.items && frm.doc.items.length > 0 &&frm.doc.items[0].item_code)
+			// following conditions assures its a follow up document						
+			if (frm.doc.__islocal && frm.doc.customer &&frm.doc.items[0].item_code)
 			{
+				
+				var items = [];
 				
 				// onload
 				// foreach items, copy current rate to customer rate and apply discount on actual rate
-				$.each(frm.doc.items, function(i, item_doc){
-
-					if (!item_doc.rate || item_doc.consoleerp_customer_rate)
-						return;
-									
-					frappe.model.set_value("Sales Invoice Item", item_doc.name, "consoleerp_customer_rate", item_doc.rate);	
+				$.each(frm.doc.items, function(i, item_doc){								
 					
-					frappe.after_ajax(function(){
+					// adding item_code in as associative array
+					items.push(item_doc.item_code);
+									
+					frappe.model.set_value("Sales Invoice Item", item_doc.name, "consoleerp_customer_rate", item_doc.rate);					
+				});
+				
+				console.log(items);
+				
+				frappe.after_ajax(function(){
 						
 						frappe.call({
 							"method" : "siyar_erpnext.api.get_customer_item_disc_percent",
 							args : {
 								customer : frm.doc.customer,
-								item : item_doc.item_code
+								items : items							
 							},
 							callback : function(r) {
-								if (!r.message)
-									r.message = 0;
-								
-								frappe.model.set_value("Sales Invoice Item", item_doc.name, "consoleerp_customer_disc_percent", r.message);		
-								frappe.model.set_value("Sales Invoice Item", item_doc.name, "rate", item_doc.consoleerp_customer_rate * (100 - item_doc.consoleerp_customer_disc_percent) / 100);							
+								console.log(r.message);
+								$.each(frm.doc.items, function(i, item_doc){										
+										frappe.model.set_value("Sales Invoice Item", item_doc.name, "consoleerp_customer_disc_percent", r.message[i]);	
+										frappe.model.set_value("Sales Invoice Item", item_doc.name, "rate", item_doc.consoleerp_customer_rate * (100 - item_doc.consoleerp_customer_disc_percent) / 100);											
+								});																														
 							}
 						});
 						
 					});
-				});
 			}
 		});
 	},
