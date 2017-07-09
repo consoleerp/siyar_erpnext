080bd5d9a13b7ad358e27710d16b7ca9b4f648c8
Console Admin <faztp12@gmail.com>
2017-07-09 08:39:31 +0300

    Due date fixes

--- siyar_erpnext/customizations/customer_group/custom_day_of_custom_month.js
+++ siyar_erpnext/customizations/customer_group/custom_day_of_custom_month.js
@@ -2,43 +2,36 @@
 
 frappe.ui.form.on(cur_frm.doctype, {
 	// updates when customer or posting_date is changed
-	customer: function(frm){
-		update_due_date(frm);
-	},
-	posting_date: function(frm) {		
-		setTimeout(function() {    
-			update_due_date(frm);
-		}, 500);
-		
-	}
-	
+	customer: function(frm) { update_due_date(frm); },
+	posting_date: function(frm) { update_due_date(frm); }
 });
 
 var update_due_date = function(frm) {
-	
-	// if due date isnt applicable, return
-	if (frm.doc.is_pos || !frm.doc.customer)
-		return;
-		
-	// waiting till the system fetches the default
-	frappe.after_ajax(function(){
-		
-		// this will call a py function, checks if 
-		// for the customer group, credit mode == custom day of custom month
-		// 	bring new date
-		// else return null and dont touch any date			
-		frappe.call({
-			method : "siyar_erpnext.customizations.customer_group.get_due_date",
-			args : {
-				posting_date: frm.doc.posting_date,
-				customer: frm.doc.customer
-			},	
-			callback : function(r)
-			{
-				if (r.message)
-					frm.set_value("due_date", r.message);
-			}
+	setTimeout(function() {
+		// if due date isnt applicable, return
+		if (frm.doc.is_pos || !frm.doc.customer)
+			return;
+
+		// waiting till the system fetches the default
+		frappe.after_ajax(function(){
+
+			// this will call a py function, checks if
+			// for the customer group, credit mode == custom day of custom month
+			// 	bring new date
+			// else return null and dont touch any date
+			frappe.call({
+				method : "siyar_erpnext.customizations.customer_group.get_due_date",
+				args : {
+					posting_date: frm.doc.posting_date,
+					customer: frm.doc.customer
+				},
+				callback : function(r)
+				{
+					if (r.message)
+						frm.set_value("due_date", r.message);
+				}
+			});
+
 		});
-		
-	});
+	}, 1000);
 };
