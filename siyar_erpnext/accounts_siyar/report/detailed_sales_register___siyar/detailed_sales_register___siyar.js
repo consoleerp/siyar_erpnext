591f1f9497a2e1fc8823430fce1f7e9efad3a164
Console Admin <faztp12@gmail.com>
2018-02-09 08:02:43 +0300

    Detailed Sales Register

---
+++ siyar_erpnext/accounts_siyar/report/detailed_sales_register___siyar/detailed_sales_register___siyar.js
@@ -0,0 +1,45 @@
+// Copyright (c) 2016, ConsoleERP and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Detailed Sales Register - Siyar"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"width": "80"
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today()
+		},
+		{
+			"fieldname":"sales_invoice",
+			"label": __("Sales Invoice"),
+			"fieldtype": "Link",
+			"options": "Sales Invoice",
+			"get_query": function() {
+				return {
+					"filters": {"docstatus" : 1}
+				}
+			}
+		},
+		{
+			"fieldname":"customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer"
+		},
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company")
+		}
+	]
+}
---
+++ siyar_erpnext/accounts_siyar/report/detailed_sales_register___siyar/detailed_sales_register___siyar.py
@@ -0,0 +1,98 @@
+# Copyright (c) 2013, ConsoleERP and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.utils import getdate, flt
+
+def execute(filters=None):
+	validate_filers(filters)
+	columns, data = get_columns(), get_data(filters)
+	return columns, data
+
+def validate_filers(filters):
+	if not filters.from_date:
+		frappe.throw("From Date is mandatory")
+	
+	if not filters.to_date:
+		frappe.throw("To Date is mandatory")
+
+def get_data(filters):
+	data = []
+	invoices = get_invoices(filters)
+	for invoice in invoices:
+		data = process_invoice_items(invoice, data)
+	return data
+	
+def process_invoice_items(invoice, data):
+	month = invoice.posting_date.strftime("%b")
+	for item in get_invoice_items(invoice):
+		vat_amount = get_item_tax(invoice, item)
+		rebate_value = flt(item.consoleerp_original_amt - item.amount) or 0 # rebate value
+		valuation_rate = flt(get_valuation_rate(invoice, item)) * item.conversion_factor
+		profit = item.amount - (valuation_rate * item.qty)
+		data.append([
+			invoice.name,
+			month,
+			invoice.posting_date,
+			invoice.po_no,
+			invoice.customer,
+			item.item_code + ": " + item.item_name,
+			item.qty, # qty, not taking stock_qty
+			item.consoleerp_customer_rate, # unit rate
+			item.consoleerp_original_amt, # customer total
+			vat_amount,
+			item.consoleerp_original_amt + vat_amount,
+			item.consoleerp_customer_disc_percent, # rebate %
+			rebate_value,
+			item.amount,
+			valuation_rate,
+			valuation_rate * item.qty,
+			profit,
+			profit / item.amount * 100
+		])
+	
+	return data
+
+def get_columns():
+	return [
+		"Invoice No:Link/Sales Invoice:120",
+		"Month::80",
+		"Posting Date:Date:90",
+		"PO No.::60",
+		"Customer:Link/Customer:120",
+		"Description::180",
+		"Qty:Float:60",
+		"U/Price:Currency/currency:80",
+		"Total:Currency/currency:80",
+		"Vat Amount:Currency/currency:80",
+		"Amount With:Currency/currency:80",
+		"Rebate %:Float:80",
+		"R/ Value:Currency/currency:80",
+		"Net Value:Currency/currency:80",
+		"P/Kg:Currency/currency:80",
+		"P/Value:Currency/currency:80",
+		"Profit:Currency/currency:80",
+		"Profit %:Float:80"
+	]
+	
+def get_valuation_rate(invoice, item):
+	valuation_rate = frappe.db.sql("""
+		SELECT valuation_rate FROM `tabStock Ledger Entry`
+		where item_code = %(item_code)s and warehouse = %(warehouse)s and valuation_rate > 0
+		 and timestamp(posting_date, posting_time) < timestamp(%(posting_date)s, %(posting_time)s)
+		order by posting_date desc, posting_time desc, name desc limit 1
+	""", {"item_code": item.item_code, "warehouse": item.warehouse, "posting_date": invoice.posting_date, "posting_time": invoice.posting_time}, debug=True)
+	return valuation_rate[0][0] if valuation_rate else 0
+
+def get_item_tax(invoice, item):
+	return item.consoleerp_original_amt / invoice.consoleerp_customer_total * invoice.total_taxes_and_charges
+
+def get_invoices(filters):
+	_filters = {"posting_date": ("between", [filters.from_date, filters.to_date]), "docstatus": 1}
+	if filters.sales_invoice:
+		_filters["name"] = filters.sales_invoice
+	return frappe.get_all("Sales Invoice", fields=["*"], filters=_filters)
+
+def get_invoice_items(invoice):
+	return frappe.get_all("Sales Invoice Item", fields=["*"], filters={"parent": invoice.name})
